f1f08cc3ac4eaf89d1bdda27e24913e1aca7da9b
Lachlan David Tailor Anderson Youngblut Esquire <ldyoungblut@gmail.com>
2022-10-19 21:27:38 -0400

    Tried my best

--- src/main/java/frc/robot/subsystems/DrivetrainSubsystem.java
+++ src/main/java/frc/robot/subsystems/DrivetrainSubsystem.java
@@ -72,9 +72,9 @@ public class DrivetrainSubsystem extends SubsystemBase {
 
   // These are our modules. We initialize them in the constructor.
   private final SwerveModule m_frontLeftModule;
-//   private final SwerveModule m_frontRightModule;
-//   private final SwerveModule m_backLeftModule;
-//   private final SwerveModule m_backRightModule;
+  private final SwerveModule m_frontRightModule;
+  private final SwerveModule m_backLeftModule;
+  private final SwerveModule m_backRightModule;
 
   private ChassisSpeeds m_chassisSpeeds = new ChassisSpeeds(0.0, 0.0, 0.0);
 
@@ -119,38 +119,38 @@ public class DrivetrainSubsystem extends SubsystemBase {
     );
 
     // We will do the same for the other modules
-//     m_frontRightModule = Mk3SwerveModuleHelper.createFalcon500(
-//             tab.getLayout("Front Right Module", BuiltInLayouts.kList)
-//                     .withSize(2, 4)
-//                     .withPosition(2, 0),
-//             Mk3SwerveModuleHelper.GearRatio.FAST,
-//             FRONT_RIGHT_MODULE_DRIVE_MOTOR,
-//             FRONT_RIGHT_MODULE_STEER_MOTOR,
-//             FRONT_RIGHT_MODULE_STEER_ENCODER,
-//             FRONT_RIGHT_MODULE_STEER_OFFSET
-//     );
+    m_frontRightModule = Mk3SwerveModuleHelper.createFalcon500(
+            tab.getLayout("Front Right Module", BuiltInLayouts.kList)
+                    .withSize(2, 4)
+                    .withPosition(2, 0),
+            Mk3SwerveModuleHelper.GearRatio.FAST,
+            FRONT_RIGHT_MODULE_DRIVE_MOTOR,
+            FRONT_RIGHT_MODULE_STEER_MOTOR,
+            FRONT_RIGHT_MODULE_STEER_ENCODER,
+            FRONT_RIGHT_MODULE_STEER_OFFSET
+    );
 
-//     m_backLeftModule = Mk3SwerveModuleHelper.createFalcon500(
-//             tab.getLayout("Back Left Module", BuiltInLayouts.kList)
-//                     .withSize(2, 4)
-//                     .withPosition(4, 0),
-//             Mk3SwerveModuleHelper.GearRatio.FAST,
-//             BACK_LEFT_MODULE_DRIVE_MOTOR,
-//             BACK_LEFT_MODULE_STEER_MOTOR,
-//             BACK_LEFT_MODULE_STEER_ENCODER,
-//             BACK_LEFT_MODULE_STEER_OFFSET
-//     );
+    m_backLeftModule = Mk3SwerveModuleHelper.createFalcon500(
+            tab.getLayout("Back Left Module", BuiltInLayouts.kList)
+                    .withSize(2, 4)
+                    .withPosition(4, 0),
+            Mk3SwerveModuleHelper.GearRatio.FAST,
+            BACK_LEFT_MODULE_DRIVE_MOTOR,
+            BACK_LEFT_MODULE_STEER_MOTOR,
+            BACK_LEFT_MODULE_STEER_ENCODER,
+            BACK_LEFT_MODULE_STEER_OFFSET
+    );
 
-//     m_backRightModule = Mk3SwerveModuleHelper.createFalcon500(
-//             tab.getLayout("Back Right Module", BuiltInLayouts.kList)
-//                     .withSize(2, 4)
-//                     .withPosition(6, 0),
-//             Mk3SwerveModuleHelper.GearRatio.FAST,
-//             BACK_RIGHT_MODULE_DRIVE_MOTOR,
-//             BACK_RIGHT_MODULE_STEER_MOTOR,
-//             BACK_RIGHT_MODULE_STEER_ENCODER,
-//             BACK_RIGHT_MODULE_STEER_OFFSET
-//     );
+    m_backRightModule = Mk3SwerveModuleHelper.createFalcon500(
+            tab.getLayout("Back Right Module", BuiltInLayouts.kList)
+                    .withSize(2, 4)
+                    .withPosition(6, 0),
+            Mk3SwerveModuleHelper.GearRatio.FAST,
+            BACK_RIGHT_MODULE_DRIVE_MOTOR,
+            BACK_RIGHT_MODULE_STEER_MOTOR,
+            BACK_RIGHT_MODULE_STEER_ENCODER,
+            BACK_RIGHT_MODULE_STEER_OFFSET
+    );
   }
 
   /**
@@ -189,8 +189,8 @@ public class DrivetrainSubsystem extends SubsystemBase {
     SwerveDriveKinematics.desaturateWheelSpeeds(states, MAX_VELOCITY_METERS_PER_SECOND);
 
     m_frontLeftModule.set(states[0].speedMetersPerSecond / MAX_VELOCITY_METERS_PER_SECOND * MAX_VOLTAGE, states[0].angle.getRadians());
-//     m_frontRightModule.set(states[1].speedMetersPerSecond / MAX_VELOCITY_METERS_PER_SECOND * MAX_VOLTAGE, states[1].angle.getRadians());
-//     m_backLeftModule.set(states[2].speedMetersPerSecond / MAX_VELOCITY_METERS_PER_SECOND * MAX_VOLTAGE, states[2].angle.getRadians());
-//     m_backRightModule.set(states[3].speedMetersPerSecond / MAX_VELOCITY_METERS_PER_SECOND * MAX_VOLTAGE, states[3].angle.getRadians());
+    m_frontRightModule.set(states[1].speedMetersPerSecond / MAX_VELOCITY_METERS_PER_SECOND * MAX_VOLTAGE, states[1].angle.getRadians());
+    m_backLeftModule.set(states[2].speedMetersPerSecond / MAX_VELOCITY_METERS_PER_SECOND * MAX_VOLTAGE, states[2].angle.getRadians());
+    m_backRightModule.set(states[3].speedMetersPerSecond / MAX_VELOCITY_METERS_PER_SECOND * MAX_VOLTAGE, states[3].angle.getRadians());
   }
 }
