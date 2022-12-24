e750220cfc2b7962c0c790b74d4fc9438e822494
symonssoftware <56047819+symonssoftware@users.noreply.github.com>
2022-12-24 12:43:25 -0500

    Modified CAN IDs for CANCoders
    In yet another attempt to get rid of the "dead wheel" issue at startup, reduced the CANCoder IDs to 10-13 so that they are higher priority devices on the CAN bus.

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -30,31 +30,36 @@ public final class Constants {
      */
     public static final double DRIVETRAIN_WHEELBASE_METERS = 0.51; // Measure and set wheelbase
 
-    public static final int DRIVETRAIN_PIGEON_ID = 61; // Set Pigeon ID
+    // CAN ID Assignments
+    // Since CAN messages are prioritized based on CAN ID, we are setting the CAN Coder IDs
+    // to lower values in an attempt to get rid of the "dead wheel" issue we occasionally are
+    // seeing at startup.
+    public static final int FRONT_LEFT_MODULE_STEER_ENCODER_ID = 10; // Set front left steer encoder ID
+    public static final int FRONT_RIGHT_MODULE_STEER_ENCODER_ID = 11; // Set front right steer encoder ID
+    public static final int BACK_LEFT_MODULE_STEER_ENCODER_ID = 12; // Set back left steer encoder ID
+    public static final int BACK_RIGHT_MODULE_STEER_ENCODER_ID = 13; // Set back right steer encoder ID
 
     public static final int FRONT_LEFT_MODULE_DRIVE_MOTOR_ID = 47; // Set front left module drive motor ID
     public static final int FRONT_LEFT_MODULE_STEER_MOTOR_ID = 48; // Set front left module steer motor ID
-    public static final int FRONT_LEFT_MODULE_STEER_ENCODER_ID = 49; // Set front left steer encoder ID
     public static final double FRONT_LEFT_MODULE_STEER_OFFSET = -Math.toRadians(305); // FIXME Measure and set front
                                                                                         // left steer offset
 
     public static final int FRONT_RIGHT_MODULE_DRIVE_MOTOR_ID = 56; // Set front right drive motor ID
     public static final int FRONT_RIGHT_MODULE_STEER_MOTOR_ID = 57; // Set front right steer motor ID
-    public static final int FRONT_RIGHT_MODULE_STEER_ENCODER_ID = 58; // Set front right steer encoder ID
     public static final double FRONT_RIGHT_MODULE_STEER_OFFSET = -Math.toRadians(163.38); // FIXME Measure and set front
                                                                                          // right steer offset
 
     public static final int BACK_LEFT_MODULE_DRIVE_MOTOR_ID = 50; // Set back left drive motor ID
     public static final int BACK_LEFT_MODULE_STEER_MOTOR_ID = 51; // Set back left steer motor ID
-    public static final int BACK_LEFT_MODULE_STEER_ENCODER_ID = 52; // Set back left steer encoder ID
     public static final double BACK_LEFT_MODULE_STEER_OFFSET = -Math.toRadians(124.22); // FIXME Measure and set back
                                                                                        // left steer offset
 
     public static final int BACK_RIGHT_MODULE_DRIVE_MOTOR_ID = 53; // Set back right drive motor ID
     public static final int BACK_RIGHT_MODULE_STEER_MOTOR_ID = 54; // Set back right steer motor ID
-    public static final int BACK_RIGHT_MODULE_STEER_ENCODER_ID = 55; // Set back right steer encoder ID
     public static final double BACK_RIGHT_MODULE_STEER_OFFSET = -Math.toRadians(66.04); // FIXME Measure and set back
                                                                                        // right steer offset
+                                                                                                                                                         
+    public static final int DRIVETRAIN_PIGEON_ID = 61; // Set Pigeon ID
 
     public static final double APRIL_TAG_COARSE_FORWARD_DRIVE_SPEED_IN_M_PER_S = 0.8;
     public static final double APRIL_TAG_FINE_FORWARD_DRIVE_SPEED_IN_M_PER_S = 0.5;
